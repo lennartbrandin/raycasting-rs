7f2ea2f864bf3f6115d1fc816609a4d8241d09cc
Lennart Brandin <lennart.brandin@tuhh.de>
2025-04-24 11:12:05 +0200

    Added display functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ const FIELD_SIZE: usize = 10;
 #[derive(Clone, Copy)]
 struct Object {
     display: Display, // Map representation
-    transparent: bool, // See through
+    transparent: bool, // See through?
     traversable: bool, // Walk through?
 }
 impl Object {
@@ -35,13 +35,37 @@ struct Entity {
     properties: Object,
     x: f32,
     y: f32,
+    angle: u16,
     fov: u16,
+}
+impl fmt::Display for Entity{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{} at ({}/{}): {}°, fov: {}", self.properties.display, self.x, self.y, self.angle, self.fov)
+    }
 }
 
 struct Game {
     player: Entity,
     map: Room,
 }
+impl fmt::Display for Game {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for x in 0..FIELD_SIZE {
+            for y in 0..FIELD_SIZE {
+                let display = 
+                if self.player.x.round() as usize == x  && self.player.y.round() as usize == y { 
+                    self.player.properties
+                } else { 
+                    self.map[x][y]
+                };
+                write!(f, "{} ", display)?;
+            }
+            write!(f, "\n")?;
+        }
+        write!(f, "{}", self.player)
+    }
+}
+
 
 const fn generate_room() -> Room {
     let mut room= [[EMPTY; FIELD_SIZE]; FIELD_SIZE];
@@ -57,17 +81,21 @@ const fn generate_room() -> Room {
     room
 }
 
-fn print_room() {
-    for x in 0..FIELD_SIZE {
-        for y in 0..FIELD_SIZE {
-            print!("{} ", ROOM[x][y]);
-        }
-        print!("\n")
+const fn generate_game() -> Game {
+    Game {
+        player: Entity { 
+            properties: Object { display: 'P', transparent: false, traversable: false },
+            x: (FIELD_SIZE/2) as f32,
+            y: (FIELD_SIZE/2) as f32,
+            angle: 0,
+            fov: 60,
+        },
+        map: generate_room(),
     }
 }
 
-const ROOM: Room = generate_room();
 
 fn main() {
-    print_room();
+    const GAME: Game = generate_game();
+    print!("{}", GAME);
 }
